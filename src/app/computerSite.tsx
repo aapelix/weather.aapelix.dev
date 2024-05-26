c0a486a87efc9b78643c2a88c4aa6e6edd885120
aapelix <mcaapelix@gmail.com>
2024-05-26 16:14:07 +0300

    Astro > Next

--- src/app/computerSite.tsx
+++
@@ -1,9 +0,0 @@
-export default function ComputerSite() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center font-black ">
-      <h1 className="bg-clip-text bg-gradient-to-r from-lime-500 to-gray-800">
-        Coming soonTM
-      </h1>
-    </div>
-  );
-}
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+ 'text': '#17100d',
+ 'background': '#fcfaf9',
+ 'primary': '#000000',
+ 'secondary': '#98dcff',
+ 'accent': '#464646',
+},
+		},
+	},
+	plugins: [],
+}
