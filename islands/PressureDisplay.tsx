0b9588a039b38a88a8a113a92ea11b2f63f15742
aapelix <mcaapelix@gmail.com>
2024-06-02 14:04:03 +0300

    Update PressureDisplay.tsx

--- islands/PressureDisplay.tsx
+++ islands/PressureDisplay.tsx
@@ -4,7 +4,7 @@ export default function PressureDisplay(props: { pressure: number }) {
   };
 
   return (
-    <div className="w-44 h-44 bg-black rounded-3xl m-3 text-white flex justify-center items-center flex-col">
+    <div className="md:w-44 md:h-44 w-36 h-36 bg-black rounded-3xl m-3 text-white flex justify-center items-center flex-col">
       <p>Pressure: {props.pressure}</p>
     </div>
   );
