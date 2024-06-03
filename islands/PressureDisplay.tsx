2d893f7632b71a6d5dfee911202a27d93ccd7070
aapelix <mcaapelix@gmail.com>
2024-06-03 18:26:13 +0300

    a

--- islands/PressureDisplay.tsx
+++ islands/PressureDisplay.tsx
@@ -4,8 +4,8 @@ export default function PressureDisplay(props: { pressure: number }) {
   };
 
   return (
-    <div className="md:w-44 md:h-44 w-36 h-36 bg-black rounded-3xl m-3 text-white flex justify-center items-center flex-col">
-      <p>Pressure: {props.pressure} hPa</p>
+    <div className="w-44 h-44 bg-black rounded-3xl m-3 text-white flex justify-center items-center flex-col">
+      <p>Pressure: {props.pressure}</p>
     </div>
   );
 }
