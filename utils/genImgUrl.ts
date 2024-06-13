ababf58726768016d40b1b2806ba251641547f1f
aapelix <mcaapelix@gmail.com>
2024-06-13 11:26:41 +0300

    New og image

--- utils/genImgUrl.ts
+++ utils/genImgUrl.ts
@@ -1,3 +1,33 @@
 export function generateImageUrl(weather: any) {
-  return `https://quickchart.io/chart?c={type:'radialGauge',data:{datasets:[{data:[${weather.current.temp_c}],backgroundColor:'white',textColor:'white'}]}}`;
+  const number = weather.current.temp_c;
+
+  // Chart configuration
+  const chartConfig = {
+    type: "doughnut",
+    data: {
+      datasets: [{
+        data: [weather.location.name + ": " + number + "°C"],
+        backgroundColor: ["transparent"],
+        borderColor: ["transparent"],
+      }],
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          display: true,
+          formatter: function () {
+            return number.toString();
+          },
+          color: "#FFFFFF",
+          font: {
+            size: 48,
+          },
+        },
+      },
+    },
+  };
+
+  // QuickChart API URL
+  return "https://quickchart.io/chart?c=" +
+    encodeURIComponent(JSON.stringify(chartConfig));
 }
